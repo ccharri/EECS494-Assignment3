ba1101e438ab2f66e7fbf659e534e96d862f5668
Thomas Dorsey <twdorsey@umich.edu>
2013-10-17 20:50:30 -0400

    wrote a parabolic fall time function

--- zenilib/jni/application/Utility.cpp
+++ zenilib/jni/application/Utility.cpp
@@ -3,10 +3,25 @@
 #include <algorithm>
 #include <zenilib.h>
 
+#include "Constants.h"
 #include "Game_Object.h"
 
 using namespace Zeni;
 using namespace std;
+
+float parabolicFallTime(float target_z_initial, float target_z_vel, float launch_z, float launch_z_vel)
+{
+	float a = -SPEED_OF_GRAVITY;
+	float b = launch_z_vel - target_z_vel;
+	float c = launch_z - target_z_initial;
+	// no non-complex roots
+	if(b*b - 4*a*c < 0)
+		return -1;
+	float t1 = (-b + (b*b - 4*a*c))/(2*a);
+	float t2 = (-b - (b*b - 4*a*c))/(2*a);
+	//NOTE: Assuming a remains negative, no more than one of these can be positive.
+	return max(t1, t2);
+}
 
 Game_Object* closestObject(const Zeni::Point3f& pos_, const vector<Game_Object*>& objects_)
 {
--- zenilib/jni/application/Utility.h
+++ zenilib/jni/application/Utility.h
@@ -6,6 +6,10 @@
 
 class Game_Object;
 
+
+float parabolicFallTime(float target_z_initial, float target_z_vel, float launch_z);
+//NOTE: Returns a negative number if the fall is invalid
+
 Game_Object* closestObject(const Zeni::Point3f& pos_, const std::vector<Game_Object*>& objects_);
 Game_Object* closestObjectMatching(const Zeni::Point3f& pos_, const std::vector<Game_Object*>& objects_, bool (*matchfunc)(Game_Object*));
 
