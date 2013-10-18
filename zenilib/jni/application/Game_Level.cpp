7f9e506f249cb31c9ff88d609ec20a78bfefc18e
Thomas Dorsey <twdorsey@umich.edu>
2013-10-17 20:18:42 -0400

    Removed the redundant closest enemy :D

--- zenilib/jni/application/Game_Level.cpp
+++ zenilib/jni/application/Game_Level.cpp
@@ -11,22 +11,6 @@ Game_Level::~Game_Level() {
 	});
 }
 
-Game_Object* Game_Level::getClosestEnemy(const Zeni::Point3f &loc)
-{
-	Game_Object* closest;
-	float distance = std::numeric_limits<float>::max(); // highest possible distance
-	for(Game_Object* g : enemies)
-	{
-		float newDistance = sqrt(pow(loc.x - g->getPosition().x, 2) + pow(loc.y - g->getPosition().y, 2) + pow(loc.z - g->getPosition().z, 2));
-		if(distance > newDistance)
-		{
-			closest = g;
-			distance = newDistance;
-		}
-	}
-	return closest;
-}
-
 void Game_Level::removeEnemy(Game_Object* enemy){
 	auto it = find(enemies.begin(), enemies.end(), enemy);
 	if(it != enemies.end()) enemies.erase(it);
