46612303f941c07e9058a16e6be80960c4ef6450
Thomas Dorsey <twdorsey@umich.edu>
2013-10-26 23:04:58 -0400

    Added iterative trajectory methods

--- zenilib/jni/application/Utility.cpp
+++ zenilib/jni/application/Utility.cpp
@@ -23,18 +23,52 @@ using namespace std;
 //	return Quaternion(yaw, pitch, roll);
 //}
 
-float parabolicFallTime(float target_z_initial, float target_z_vel, float launch_z, float launch_z_vel)
+
+float getTimeParabolic(float velY, float deltaY)
+//NOTE: Returns a negative value if the parabolic can't work.
 {
-	float a = -SPEED_OF_GRAVITY;
-	float b = launch_z_vel - target_z_vel;
-	float c = launch_z - target_z_initial;
-	// no non-complex roots
-	if(b*b - 4*a*c < 0)
-		return -1;
+	float a = SPEED_OF_GRAVITY;
+	float b = velY;
+	float c = deltaY;
 	float t1 = (-b + sqrt(b*b - 4*a*c))/(2*a);
 	float t2 = (-b - sqrt(b*b - 4*a*c))/(2*a);
-	//NOTE: Assuming a remains negative, no more than one of these can be positive.
-	return max(t1, t2);
+	if(t1 < 0)
+		return t2;
+	else if(t2 < 0)
+		return t1;
+	return min(t1, t2);
+}
+
+float getAngleParabolic(const Point3f targetPos, const Point3f launchPos, const float launchVel)
+//NOTE: Returns a value between pi/2 and -pi/2. If no trajectory, returns 2pi.
+{
+	float d = hypot(targetPos.x - launchPos.x, targetPos.y - launchPos.y);
+	float z = targetPos.z - launchPos.z;
+	float v = launchVel;
+	float det = v*v*v*v - SPEED_OF_GRAVITY*(SPEED_OF_GRAVITY*d*d + 2*z*v*v);
+	if(det < 0)
+		return 2*acos(1);
+	float a1 = atan2(v*v + sqrt(det), SPEED_OF_GRAVITY*d);
+	float a2 = atan2(v*v - sqrt(det), SPEED_OF_GRAVITY*d);
+	return min(a2, a1);
+}
+float getTimeIterativeParabolic(const Point3f targetPos, const Vector3f targetVel, const Point3f launchPos, const float launchVel)
+//NOTE: Returns a time, or -1 if there's no trajectory.
+{
+	Point3f iterativePos = targetPos;
+	float error;
+	do
+	{
+		float angle = getAngleParabolic(targetPos, targetVel, launchVel);
+		if(angle == 2*acos(1))
+			return -1;
+		float time = getTimeParabolic(targetPos.z - launchPos.z, sin(angle)*launchVel);
+		if(time < 0)
+			return -1;
+		Point3f newPos = targetPos + targetVel * time;
+		error = pow(iterativePos.x - newPos.x, 2) + pow(iterativePos.y - newPos.y, 2) + pow(iterativePos.z - newPos.z, 2); 
+		iterativePos = newPos;
+	}while(error < 0.25);
 }
 
 shared_ptr<Game_Object> closestObject(const Zeni::Point3f& pos_, const vector<shared_ptr<Game_Object>>& objects_)
--- zenilib/jni/application/Utility.h
+++ zenilib/jni/application/Utility.h
@@ -10,8 +10,9 @@
 
 class Game_Object;
 
-float parabolicFallTime(float target_z_initial, float target_z_vel, float launch_z);
-//NOTE: Returns a negative number if the fall is invalid
+float getTimeParabolic(const float velY, const float deltaY);
+float getAngleParabolic(const Zeni::Point3f targetPos, const Zeni::Point3f launchPos, const float launchVel);
+float getTimeIterativeParabolic(const Zeni::Point3f targetPos, const Zeni::Vector3f targetVel, const Zeni::Point3f launchPos, const float launchVel); 
 
 std::shared_ptr<Game_Object> closestObject(const Zeni::Point3f& pos_, const std::vector<std::shared_ptr<Game_Object> >& objects_);
 
